c6bb7a0fe0ba52fec1500b03fec2103b7c9fc618
Diego-Barreto-web <diegobarretopro@gmail.com>
2023-12-27 13:27:17 -0300

    Atualizações JSON

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,9 @@
 
 
-function fetchProfileData() {
-    const url = ''
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/Diego-Barreto-web/Lab-Desafio-02-JS-DIO/main/assets/profile.json'
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
 
 
